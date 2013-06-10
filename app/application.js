448aa5c79cc60bf8aa6d5c20e70784e6a69435ee
j-mcnally <justin@kohactive.com>
2013-06-10 00:32:52 -0500

    update

--- app/application.js
+++ app/application.js
@@ -53066,19 +53066,3 @@ set$(App, 'IndexRoute', Ember.Route.extend({
     return this.transitionTo('friends.index');
   }
 }));
-extends$(IndexRoute, Ember.Route);
-function isOwn$(o, p) {
-  return {}.hasOwnProperty.call(o, p);
-}
-function extends$(child, parent) {
-  for (var key in parent)
-    if (isOwn$(parent, key))
-      child[key] = parent[key];
-  function ctor() {
-    this.constructor = child;
-  }
-  ctor.prototype = parent.prototype;
-  child.prototype = new ctor;
-  child.__super__ = parent.prototype;
-  return child;
-};
